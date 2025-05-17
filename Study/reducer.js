b502d8e5f359be8e443b692f3a4b1b4e4174903d
redoy1102 <hridoyvlog9@gmail.com>
2025-05-17 16:05:25 +0600

    Updated

--- Study/reducer.js
+++ Study/reducer.js
@@ -1,12 +1,8 @@
 // Example: 1
-// const nums = [1, 2, 3, 4, 5];
-// const sum = nums.reduce((accumulator, currentValue, currentIndex, array) => {
-//   console.log(`Accumulator: ${accumulator}, Current value: ${currentValue}`);
-//   return accumulator + currentValue;
-// }, 0);
-
-// console.log("Summation:", sum);
-
-// Example: 2
-
 const nums = [1, 2, 3, 4, 5];
+const sum = nums.reduce((accumulator, currentValue, currentIndex, array) => {
+  console.log(`Accumulator: ${accumulator}, Current value: ${currentValue}`);
+  return accumulator + currentValue;
+}, 0);
+
+console.log("Summation:", sum);
---
+++ Study/splice.js
@@ -0,0 +1,5 @@
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+const result = fruits.splice(1, 3, "By Fuad");
+
+console.log(result);
+console.log(fruits);
